db6e586a9ba9527629f7ac2951c0d0058344947e
ken <ken@kendeMacBook-Air.local>
2025-12-07 11:40:14 +0800

    Add unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_add() {
+        let a = 1;
+        let b = 2;
+        assert_eq!(a + b, 3);
+    }
+}
